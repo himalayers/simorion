f8e9e93f2f192c1d56bd2101319d90e28d584311
Tom <tom.fullalove@gmail.com>
2016-02-10 14:45:02 +0000

    Update Layer.java

--- simorion/src/simorion/Layer.java
+++ simorion/src/simorion/Layer.java
@@ -2,6 +2,7 @@ package simorion;
 import java.io.Serializable;
 
 //Can serializable only be applied to the array? probably a question for sprint 3 etc.
+@author Tom Fullalove
 public class Layer implements Serializable{
     boolean[][] dots;
     
@@ -9,6 +10,7 @@ public class Layer implements Serializable{
         dots = new boolean[16][16];
     }
     
+    @author Tom Fullalove
     public boolean toggleDot(int posx, int posy){
         if (dots[posx][posy])
         {
@@ -22,6 +24,7 @@ public class Layer implements Serializable{
         }
     }
 
+    
     public void ClearDots(){
             dots = new boolean[16][16];
     }
