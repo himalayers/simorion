077aeb1781cc77f2d55d4ca9367f42934b7c3037
Tom <tom.fullalove@gmail.com>
2016-03-08 14:08:31 +0000

    Create Mode.java

---
+++ simorion/src/simorion/Mode.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui;
+
+/**
+ *
+ * @author ool201
+ */
+public interface Mode{
+    
+    //OK button functionality
+    public void OKEventAction();
+    
+    //Matrix buttons functionality
+    public void MatrixEventAction();
+}
