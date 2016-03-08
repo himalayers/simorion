878fe90e761d045ced11c3ce2380cdd2023f0f12
Tom <tom.fullalove@gmail.com>
2016-03-08 14:08:59 +0000

    Added files via upload

---
+++ simorion/src/simorion/LoopSpeedMode.java
@@ -0,0 +1,33 @@
+package gui;
+
+/**
+ *
+ * @author ool201
+ */
+public class LoopSpeedMode implements Mode{
+    private int voicenumber;
+    
+    //OK button functionality
+    @Override
+    public void OKEventAction(){
+        //change the voice
+        
+        //Tom's code
+    }
+    
+    //Matrix buttons functionality
+    @Override
+    public void MatrixEventAction(){
+        //What happens when a button is clicked?
+        
+        //get i and j
+        
+        //show that a button has been selected (call Ildens function on innerbutton)
+        
+        //what does the i and j equate to?
+        
+        //update the lcd screen
+        
+        //Wunmi's code
+    }
+}
