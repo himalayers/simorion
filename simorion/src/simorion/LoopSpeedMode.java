e0eeb9a8832e7233037d81e7a053d68d3e910a6f
woomie <wunmilawal24@gmail.com>
2016-03-09 00:03:07 +0000

    Update LoopSpeedMode.java

--- simorion/src/simorion/LoopSpeedMode.java
+++ simorion/src/simorion/LoopSpeedMode.java
@@ -1,11 +1,16 @@
 package gui;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 /**
  *
  * @author ool201
  */
-public class LoopSpeedMode implements Mode{
+public class LoopSpeedMode implements Mode, ActionListener{
     private int voicenumber;
+     private int i;
+      private int j;
     
     //OK button functionality
     @Override
@@ -16,9 +21,11 @@ public class LoopSpeedMode implements Mode{
     }
     
     //Matrix buttons functionality
-    @Override
-    public void MatrixEventAction(int i, int j){
-        //What happens when button (i,j) is clicked?
+    
+    public void MatrixEventAction(){
+        //What happens when a button is clicked?
+        
+        //get i and j
         
         //show that a button has been selected (call Ildens function on innerbutton)
         
@@ -28,4 +35,40 @@ public class LoopSpeedMode implements Mode{
         
         //Wunmi's code
     }
+
+    @Override
+    public void MatrixEventAction(int i, int j) {
+        this.i=i;
+        this.j=j;
+        GUI gui=new GUI();
+        gui.L4.addActionListener((ActionListener) this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+          GUI gui=new GUI();
+          for ( i=0; i<16; i++){
+                for( j=0; j<16; j++){
+                    //gets the button source
+               if(gui.ibutton.buttons[i][j]==ae.getSource()){
+                voicenumber=(i+(j*16));
+                
+                if(voicenumber<=160){
+               Controller.clockhand.settime(voicenumber);
+               gui.display.setText(""+ Controller.clockhand.gettime());
+                
+       }
+                //feel free to edit.same problem as velocitymode
+                else{
+                    voicenumber-=160;
+                    Controller.clockhand.settime(voicenumber);
+                   gui.display.setText(""+ Controller.clockhand.gettime());
+                
+                }
+                }
+          }
+       
+   }
+    }
 }
+
