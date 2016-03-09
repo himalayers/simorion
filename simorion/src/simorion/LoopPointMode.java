461e523f555d5feec7dbaa91399b8f09f2094628
woomie <wunmilawal24@gmail.com>
2016-03-09 00:01:34 +0000

    Update LoopPointMode.java

--- simorion/src/simorion/LoopPointMode.java
+++ simorion/src/simorion/LoopPointMode.java
@@ -1,12 +1,16 @@
-package gui;
+package Simori on;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  *
  * @author ool201
  */
-public class LoopPointMode implements Mode{
+public class LoopPointMode implements Mode, ActionListener{
     private int voicenumber;
-    
+    private int j;
+    private int i;
     //OK button functionality
     @Override
     public void OKEventAction(){
@@ -16,9 +20,10 @@ public class LoopPointMode implements Mode{
     }
     
     //Matrix buttons functionality
-    @Override
-    public void MatrixEventAction(int i, int j){
-        //What happens when button (i,j) is clicked?
+    public void MatrixEventAction(){
+        //What happens when a button is clicked?
+        
+        //get i and j
         
         //show that a button has been selected (call Ildens function on innerbutton)
         
@@ -28,4 +33,33 @@ public class LoopPointMode implements Mode{
         
         //Wunmi's code
     }
+
+    @Override
+    public void MatrixEventAction(int i, int j) {
+         this.i=i;
+        this.j=j;
+        GUI gui=new GUI();
+        gui.L4.addActionListener((ActionListener) this);
 }
+        
+    
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+           GUI gui=new GUI();
+          
+          for ( i=0; i<16; i++){
+                for( j=0; j<16; j++){
+                    //gets the button source
+               if(gui.ibutton.buttons[i][j]==ae.getSource()){
+                int col=j;
+                //this dont give the correct result
+                Controller.getCurrentLayer().setColumn(col);
+                 gui.display.setText("" +Controller.getCurrentLayer().getColumn(col));
+                }
+                }
+          }
+          }
+    }
+
+       
